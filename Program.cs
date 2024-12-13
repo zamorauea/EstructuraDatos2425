6b235487785ee5b7372ed42857e06263dd32f51d
Viviana  Zamora <159592013+zamorauea@users.noreply.github.com>
2024-12-13 18:54:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Universidad Estala Amazonia"); 
+Console.WriteLine(""); 
+
+Rectangulo figura1 = new Rectangulo (4,7); 
+Console.WriteLine("El area del rectangulo es:" + figura1. Area());
+
